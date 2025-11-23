140e0b036e9b89ed82fd9212a7eabfb1a681bb7c
Rumanzi <ndamukundavainqueur@gmail.com>
2025-11-23 23:53:47 +0200

    add new features

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,69 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <>
+//       <div>
+//         <a href="https://vite.dev" target="_blank">
+//           <img src={viteLogo} className="logo" alt="Vite logo" />
+//         </a>
+//         <a href="https://react.dev" target="_blank">
+//           <img src={reactLogo} className="logo react" alt="React logo" />
+//         </a>
+//       </div>
+//       <h1>Vite + React</h1>
+//       <div className="card">
+//         <button onClick={() => setCount((count) => count + 1)}>
+//           count is {count}
+//         </button>
+//         <p>
+//           Edit <code>src/App.jsx</code> <div className=''>and save to test HMR</div>
+//         </p>
+//       </div>
+//       <p className="read-the-docs text-2xl text-blue-500">
+//         Click on the Vite and React logos to learn more
+//       </p>
+//     </>
+//   )
+// }
+
+// export default App
+
+
+
+import Header from './Header'
+import HeroSection from './HeroSection'
+import Logobar from './Logobar'
+import Navbar from './Navbar'
+import Imisanzu from './Imisanzu'
+import Transformation from './Transformation'
+import Events from './Events'
+import Footer from './Footer'
+import Body from './Body'
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> <div className=''>and save to test HMR</div>
-        </p>
-      </div>
-      <p className="read-the-docs text-2xl text-blue-500">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+    <div >
+       <Navbar />
+       <Logobar/>
+       <Header/>
+       <HeroSection/>
+         <Body/>
+   <Imisanzu/>
+  <Transformation/>
+  <Events/>
+  <Footer/>
+    </div>
+  
+
   )
 }
 
---
+++ src/Body.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+
+function Body() {
+  return (
+    <div>
+
+
+<div className="mt-30 flex  flex-col items-center text-3xl">
+      <div className="font-bold">Optimistic and forward-looking </div>
+<div className="font-bold">agent of central government, working to achieve </div>
+    <div className="font-bold text-blue-950">the financial security and health of </div>
+             <div className="font-bold  text-blue-950">all Rwandans.</div>
+</div>
+
+
+
+
+
+<div className="flex justify-center mt-20 text-2xl text-blue-800 font-bold">
+<div className="bg-amber-300 w-7 h-1 mt-4 "></div>SOCIAL PROTECTION SCHEMES</div>
+
+
+
+{/* SOCIAL PROTECTION SCHEMES */}
+
+<div className="w-90 mt-30 ml-30">
+
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+     
+    </div>
+  );
+}
+
+export default Body;
